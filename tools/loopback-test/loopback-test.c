6384cd9ff3eaa279a8f048812d79b077644aa169
Josh Droney <joshd@commtech-fastcom.com>
2016-05-02 15:45:11 -0500

    Cleaned up the project properties, created a new tools directory for useful tools.

--- tools/loopback-test/loopback-test.c
+++ tools/loopback-test/loopback-test.c
@@ -4,21 +4,40 @@
 #include "../../inc/synccom.h"
 #include "../../inc/calculate_clock_bits.h"
 
-int main(void)
+#define MAX_BUFFER_SIZE 5000
+
+int main(int argc, char *argv[])
 {
 	HANDLE h = 0;
 	DWORD bytes_written, bytes_read, tmp;
-	unsigned char odata[5000], idata[5000];
+	unsigned char odata[MAX_BUFFER_SIZE], idata[MAX_BUFFER_SIZE+10], handle_string[20], clock_bits[20];
 	struct synccom_registers regs;
-	int num_loops = 50, write_size = 100, errors = 0, j, i;
-	unsigned char clock_bits[20];
+	int write_size = 0, errors = 0, j, i;
+	size_t num_loops= 0, max_size= 0;
 
-	/* Open port 0 in a blocking IO mode */
-	h = CreateFile(L"\\\\.\\SYNCCOM0", GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0, NULL);
-	if (h == INVALID_HANDLE_VALUE) {
-		fprintf(stderr, "CreateFile failed with %d\n", GetLastError());
+	// Verify the user is providing enough information.
+	if(argc != 4)
+	{
+		printf("Usage:\n\t%s [port number] [number of loops] [max size]\n", argv[0]);
 		return EXIT_FAILURE;
 	}
+	// Open the port for read/write.
+	sprintf_s(handle_string, 20, "\\\\.\\SYNCCOM%d", atoi(argv[1]));
+	printf("%s\n", handle_string);
+	h = CreateFileA(handle_string, GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0, NULL);
+	if (h == INVALID_HANDLE_VALUE) {
+		fprintf(stderr, "CreateFile failed with %d\n", GetLastError());
+		CloseHandle(h);
+		return EXIT_FAILURE;
+	}
+	num_loops = atoi(argv[2]);
+	max_size = atoi(argv[3]);
+	if (max_size > sizeof(odata)) {
+		printf("Supplied size is too large! Setting maximum write size to %d!", sizeof(odata));
+		max_size = sizeof(odata);
+	}
+
+	// These are standard HDLC settings.
 	SYNCCOM_REGISTERS_INIT(regs);
 	regs.FIFOT = 0x00040004;
 	regs.CCR0 = 0x00112004;
@@ -36,18 +55,32 @@ int main(void)
 	regs.IMR = 0x0f000000;
 	regs.DPLLR = 0x00000004;
 	DeviceIoControl(h, SYNCCOM_SET_REGISTERS, &regs, sizeof(regs), NULL, 0, &tmp, (LPOVERLAPPED)NULL);
+
+	// Because the BGR register is 0, the clock is set to this value; 6,000,000.
 	calculate_clock_bits(6000000, 10, clock_bits);
 	DeviceIoControl(h, SYNCCOM_SET_CLOCK_BITS, &clock_bits, sizeof(clock_bits), NULL, 0, &tmp, (LPOVERLAPPED)NULL);
+
+	// Always purge after potentially changing the 'mode' bits in the CCR0 register.
 	DeviceIoControl(h, SYNCCOM_PURGE_TX, NULL, 0, NULL, 0, &tmp, (LPOVERLAPPED)NULL);
 	DeviceIoControl(h, SYNCCOM_PURGE_RX, NULL, 0, NULL, 0, &tmp, (LPOVERLAPPED)NULL);
-	printf("Testing! Please wait.\n\n");
+	
 	// Currently because of the nature of USB you cannot be sure when the PURGE_TX and PURGE_RX has completed.
+	// There is no return value for these commands, so we can only try and hope for the best.
 	// This seems adequate in making sure they complete.
 	Sleep(1000);
-	for (i = 0; i < num_loops; i++)
+
+	printf("Testing %d loops! Please wait.\n\n", num_loops);
+	for (i = 0; i < (int)num_loops; i++)
 	{
+		// Generate random data of a random size.
+		write_size = rand() % max_size;
 		for (j = 0; j < write_size; j++) odata[j] = rand() % 255;
+		
+		// Now we write and verify we wrote the correct amount.
 		WriteFile(h, odata, write_size, &bytes_written, NULL);
+		if (bytes_written != write_size) printf("%d: Failed to write the total frame! Attmpted to write %d, wrote %d.\n", write_size, bytes_written);
+
+		// Read the data and verify that it's correct.
 		ReadFile(h, idata, sizeof(idata), &bytes_read, NULL);
 		if (bytes_written != bytes_read) {
 			printf("%d: Didn't read the correct number of bytes! Wrote %d bytes, read %d bytes!\n", i, bytes_written, bytes_read);
@@ -57,11 +90,13 @@ int main(void)
 			printf("%d: Data mismatch: byte %d: 0x%2.2x != 0x%2.2x\n", i, j, odata[j], idata[j]);
 			errors++;
 		}
+
+		// Once we have an error, we break.
 		if (errors) break;
 	}
 
+	// Always close your handle when you're done using the port.
 	printf("Test complete! %d errors!\n", errors);
-
 	CloseHandle(h);
 
 	return 0;
---
+++ tools/reprogram-firmware/reprogram-firmware.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <Windows.h>
+#include "../../inc/synccom.h"
+
+// Just for reference, I'll keep this here.
+#define CURRENT_FIRMWARE_FILE "sfscc2usb_Rev_010D.s19"
+
+int main(int argc, char *argv[])
+{
+	HANDLE h = 0;
+	DWORD tmp;
+	unsigned char firmware_line[50], handle_string[20];
+	FILE *firmware_file;
+	int i = 0;
+	errno_t err;
+
+	if (argc != 3)
+	{
+		printf("Usage:\n\t%s [port number] [firmware file]\n", argv[0]);
+		return EXIT_FAILURE;
+	}
+	sprintf_s(handle_string, 20, "\\\\.\\SYNCCOM%d", atoi(argv[1]));
+	printf("%s\n", handle_string);
+	h = CreateFileA(handle_string, GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0, NULL);
+	if (h == INVALID_HANDLE_VALUE) {
+		fprintf(stderr, "CreateFile failed with %d\n", GetLastError());
+		CloseHandle(h);
+		return EXIT_FAILURE;
+	}
+	err = fopen_s(&firmware_file, argv[2], "r");
+	if (err != 0) {
+		fprintf(stderr, "fopen failed file %s with: %d\n", argv[2], GetLastError());
+		CloseHandle(h);
+		return EXIT_FAILURE;
+	}
+	if (fgets(firmware_line, 50, firmware_file) == NULL)
+	{
+		fprintf(stderr, "failed to read line: %d\n", GetLastError());
+		CloseHandle(h);
+		fclose(firmware_file);
+		return EXIT_FAILURE;
+	}
+
+	// A sleep is required here - there is no way to be certain that the erase completed, but a 5s sleep should be more
+	// than adequate.
+	// The first line of each program file is always an 'erase' command.
+	printf("Erasing firmware...");
+	DeviceIoControl(h, SYNCCOM_REPROGRAM, &firmware_line, sizeof(firmware_line), NULL, 0, &tmp, (LPOVERLAPPED)NULL);
+	Sleep(5000);
+	printf("Firmware erased!\n");
+	printf("Programming..");
+	
+	// We can just write line by line - nothing really special is needed.
+	while (1) {
+		i++;
+		if (fgets(firmware_line, 50, firmware_file) == NULL) break;
+		DeviceIoControl(h, SYNCCOM_REPROGRAM, &firmware_line, sizeof(firmware_line), NULL, 0, &tmp, (LPOVERLAPPED)NULL);
+		if (i > 100000) {
+			printf("File exceeded 100,000 lines. May not be correct! Programming failed, exiting..\n");
+			break;
+		}
+	}
+
+	// Unfortunately there's no way to verify success other than testing.
+	printf("\n\nProgramming finished! Please power cycle the device and test it before using it.\n");
+
+	fclose(firmware_file);
+	CloseHandle(h);
+	return EXIT_SUCCESS;
+}
